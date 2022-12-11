deb8cbfb05695649ab6dd99e565e11869ec6bf2b
Mona Lisa <johndoe@example.com>
2022-12-11 16:03:58 +1000

    huoiohj

--- hash.js
+++ hash.js
@@ -1,8 +1,5 @@
 import { readFileSync } from 'node:fs'
 import { createHmac, randomUUID, scrypt, scryptSync } from 'node:crypto'
-import { hrtime } from 'node:process';
-import { waitForDebugger } from 'node:inspector';
-import { scheduler } from 'node:timers/promises';
 const secret = 'abcdefg';
 const sha256 = (data, code) => createHmac('sha256', secret).update(data).digest(code)
 
@@ -10,10 +7,10 @@ const sha256 = (data, code) => createHmac('sha256', secret).update(data).digest(
 const fileContent = readFileSync('Дым и Комок.txt', 'utf8')
 
 const hash = sha256(fileContent, 'hex')
-console.log(hash)
+console.log(hash, '\n')
 
 const hash1 = sha256(fileContent.slice(0, -1), 'hex')
-console.log(hash1)
+console.log(hash1, '\n')
 
 
 const password = 'random password'
@@ -39,6 +36,55 @@ const payload = Buffer.from(JSON.stringify({
 const signature = sha256(`${head}.${payload}`, 'base64')
 
 console.log(`\n\n\nTOKEN IS: ${head}.${payload}.${signature}`);
+
+
+// class Block {
+//     constructor(timestamp = "", data = []) {
+//         this.timestamp = timestamp;
+//         this.data = data;
+//         this.hash = this.getHash();
+//         this.prevHash = ""; // хеш предыдущего блока
+//     }
+
+//     // Наша хеш-функция.
+//     getHash() {
+//         return sha256(this.prevHash + this.timestamp + JSON.stringify(this.data), 'hex');
+//     }
+// }
+
+// class Blockchain {
+//     constructor() {
+//         // Создаём первичный блок
+//         this.chain = [new Block(Date.now().toString())]
+//     }
+//     getLastBlock() {
+//         return this.chain[this.chain.length - 1];
+//     }
+//     addBlock(block) {
+//         // Так как мы добавляем новый блок, prevHash будет хешем предыдущего последнего блока
+//         block.prevHash = this.getLastBlock().hash;
+//         // Так как теперь в prevHash имеется значение, нужно пересчитать хеш блока
+//         block.hash = block.getHash();
+//         this.chain.push(block);
+//     }
+//     isValid() {
+//         // Перед перебором цепочки блоков нужно установить i в 1, так как до первичного блока никаких блоков нет. В результате мы начинаем со второго блока.
+//         for (let i = 1; i < this.chain.length; i++) {
+//             const currentBlock = this.chain[i];
+//             const prevBlock = this.chain[i-1];
+
+//             // Проверка
+//             if (currentBlock.hash !== currentBlock.getHash() || prevBlock.hash !== currentBlock.prevHash) {
+//                 return false;
+//             }
+//         }
+
+//         return true;
+//     }
+// }
+
+// console.log(new Blockchain().isValid())
+
 
 
 
