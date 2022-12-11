e7a9ffbdc8caadb2f1b19984066f24e3816995e3
Mona Lisa <johndoe@example.com>
2022-12-11 14:44:36 +1000

    kllk

--- hash.js
+++ hash.js
@@ -1,38 +1,44 @@
-import { readFileSync} from 'node:fs'
-
-import { createHmac, randomUUID } from 'node:crypto'
-import { encode } from 'node:punycode';
-
-let fileContent = readFileSync('Дым и Комок.txt', 'utf8')
-// console.log(fileContent)
+import { readFileSync } from 'node:fs'
+import { createHmac, randomUUID, scrypt, scryptSync } from 'node:crypto'
+import { hrtime } from 'node:process';
+import { waitForDebugger } from 'node:inspector';
+import { scheduler } from 'node:timers/promises';
 const secret = 'abcdefg';
-const hash = createHmac('sha256', secret)
-               .update(fileContent)
-               .digest('hex')
-console.log(hash)
-// console.log(fileContent.slice(0, -1))
+const sha256 = (data, code) => createHmac('sha256', secret).update(data).digest(code)
 
-const hash1 = createHmac('sha256', secret)
-               .update(fileContent.slice(0, -1))
-               .digest('hex')
+
+const fileContent = readFileSync('Дым и Комок.txt', 'utf8')
+
+const hash = sha256(fileContent, 'hex')
+console.log(hash)
+
+const hash1 = sha256(fileContent.slice(0, -1), 'hex')
 console.log(hash1)
 
-const hash2 = createHmac('sha256', secret)
-               .update('password')
-               .digest('hex')
+
+const password = 'random password'
+
+console.time('sha256')
+const hash2 = sha256(password, 'hex')
 console.log(hash2)
+console.timeEnd('sha256');
+
+console.time('scrypt')
+const hash22 = scryptSync(password, 'salt', 32).toString('hex')
+console.log(hash22)
+console.timeEnd('scrypt')
+
+
+
 
 const head = Buffer.from(JSON.stringify({ alg: 'HS256', typ: 'jwt' })).toString('base64')
 const payload = Buffer.from(JSON.stringify({
     id: randomUUID(),
     data: ['some data']
 })).toString('base64')
-const signature = createHmac('SHA256', secret)
-        .update(`${head}.${payload}`)
-        .digest('base64')
+const signature = sha256(`${head}.${payload}`, 'base64')
 
 console.log(`\n\n\nTOKEN IS: ${head}.${payload}.${signature}`);
-
 
 
 
